c02638f980de1d2e1b52ffef09a78a0751cf0edf
nailahgucon <nailahgucon@gmail.com>
2024-02-05 22:27:12 +0800

    refactor backend code

--- cre8AI/add_text.py
+++ cre8AI/add_text.py
@@ -1,37 +1,29 @@
 from PIL import Image, ImageDraw, ImageFont
 
-def add_text_to_panel(text, panel_image):
-  text_image = generate_text_image(text)
+def addText(text, panel):
+  # call generatePanelWithText
+  panelWithText = generatePanelWithText(text)
 
-  result_image = Image.new('RGB', (panel_image.width, panel_image.height + text_image.height))
+  result = Image.new('RGB', (panel.width, panel.height + panelWithText.height))
+  result.paste(panel, (0, 0))
+  result.paste(panelWithText, (0, panel.height))
 
-  result_image.paste(panel_image, (0, 0))
+  return result
 
-  result_image.paste(text_image, (0, panel_image.height))
-
-  return result_image
-
-def generate_text_image(text):
+def generatePanelWithText(text):
     # Define image dimensions
     width = 1024
     height = 128
 
     # Create a white background image
     image = Image.new('RGB', (width, height), color='white')
-
-    # Create a drawing context
     draw = ImageDraw.Draw(image)
 
-    font = ImageFont.truetype(font="Friendszone.ttf", size=30)
-
-    # Calculate the position to center the text horizontally and vertically
+    # text details
+    font = ImageFont.truetype(font="Friendszone.ttf", size=50)
     x = 10
     y = 10
-
-    # Define text color (black in this example)
     text_color = (0, 0, 0)
-
-    # Add text to the image
     draw.text((x, y), text, fill=text_color, font=font)
 
     return image
--- cre8AI/stability_ai.py
+++ cre8AI/stability_ai.py
@@ -7,22 +7,20 @@ from PIL import Image
 from stability_sdk import client
 import stability_sdk.interfaces.gooseai.generation.generation_pb2 as generation
 
+# insert your own STABILITY_KEY
+# site to obtain STABILITY_KEY: https://platform.stability.ai/account/keys
 os.environ['STABILITY_HOST'] = 'grpc.stability.ai:443'
 os.environ['STABILITY_KEY'] = 'sk-1niz077MXYbZFlTt0jtrsUlpK9hS2iIWieIHyy3yEMm2qAAh'
 
+# set random seed
 seed = random.randint(0, 1000000000)
 
-# Set up our connection to the API.
-stability_api = client.StabilityInference(
-    key=os.environ['STABILITY_KEY'],
-    verbose=True,
-    engine="stable-diffusion-xl-1024-v1-0"
-)
+# Set up our connection to API
+stability_api = client.StabilityInference(key=os.environ['STABILITY_KEY'], verbose=True, engine="stable-diffusion-xl-1024-v1-0")
 
-def text_to_image(prompt):
+def generateImageFromText(prompt):
     # Set up our initial generation parameters.
-    answers = stability_api.generate(
-        # prompt="rocket ship launching from forest with flower garden under a blue sky, masterful, ghibli",
+    result = stability_api.generate(
         prompt=prompt,
         seed=seed,
         steps=30,
@@ -32,39 +30,11 @@ def text_to_image(prompt):
         sampler=generation.SAMPLER_K_DPMPP_2M
     )
 
-    for resp in answers:
-        for artifact in resp.artifacts:
+    for response in result:
+        for artifact in response.artifacts:
             if artifact.finish_reason == generation.FILTER:
-                warnings.warn(
-                    "Your request activated the API's safety filters and could not be processed."
-                    "Please modify the prompt and try again.")
+                warnings.warn("Your request activated the API's safety filters and could not be processed. Please modify the prompt and try again.")
             if artifact.type == generation.ARTIFACT_IMAGE:
-                global img
-                img = Image.open(io.BytesIO(artifact.binary))
-                return img 
-
-def edit_image(input_image_path, prompt, output_image_name):
-    img = Image.open(input_image_path)
-
-    answers = stability_api.generate(
-        prompt=prompt,
-        init_image=img,
-        start_schedule=0.6,
-        seed=123463446,
-        steps=50,
-        cfg_scale=8.0,
-        width=512,
-        height=512,
-        sampler=generation.SAMPLER_K_DPMPP_2M
-    )
-
-    for resp in answers:
-        for artifact in resp.artifacts:
-            if artifact.finish_reason == generation.FILTER:
-                warnings.warn(
-                    "Your request activated the API's safety filters and could not be processed."
-                    "Please modify the prompt and try again.")
-            if artifact.type == generation.ARTIFACT_IMAGE:
-                global img2
-                img2 = Image.open(io.BytesIO(artifact.binary))
-                img2.save(output_image_name + ".png")
+                global image
+                image = Image.open(io.BytesIO(artifact.binary))
+                return image
